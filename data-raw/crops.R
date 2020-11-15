87e6eb00d387dd988f660b6680601469d4c54e03
aftoncoombs <aftoncoombs@gmail.com>
2020-11-15 15:31:22 -0800

    Cleaned crop data format
    - Split growth_seasons into 4 boolean-valued columns, one per season
    - Split tint_color into 2 columns, a boolean for having tint color and a value for tint, if applicable
    - Moved the convenience load_all script to data-raw where it belongs

--- data-raw/crops.R
+++ data-raw/crops.R
@@ -2,6 +2,25 @@
 
 crops <-
   readr::read_csv(file = "data-raw/files/Crops.csv") %>%
-  make_cols_snake_case()
+  make_cols_snake_case() %>%
+  dplyr::mutate(growth_season_spring =
+                  grepl(pattern = "spring", x = growth_seasons),
+                growth_season_summer =
+                  grepl(pattern = "summer", x = growth_seasons),
+                growth_season_fall =
+                  grepl(pattern = "fall", x = growth_seasons),
+                growth_season_winter =
+                  grepl(pattern = "winter", x = growth_seasons)) %>%
+  dplyr::select(-growth_seasons) %>%
+  tidyr::separate(col = tint_color,
+                  into = c("tint_color", "tint_color_value"),
+                  sep = " ",
+                  extra = "merge",
+                  fill = "right") %>%
+  dplyr::mutate(tint_color = as.logical(tint_color)) %>%
+  dplyr::select(objectid,
+                dplyr::starts_with(match = "days_in_stage"),
+                dplyr::starts_with(match = "growth_season"),
+                dplyr::everything())
 
 usethis::use_data(crops, overwrite = TRUE)
