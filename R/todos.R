56956a0e9a122d54682edce2ca6589538425acf2
aftoncoombs <aftoncoombs@gmail.com>
2021-05-20 14:54:49 -0700

    Added new Objects dataset
    - Added new Objects dataset as a .csv + updated stardewdata jar
    - Added some TODO's related to ingesting the new dataset

--- R/todos.R
+++ R/todos.R
@@ -1,4 +1,8 @@
 ## TODO's
+## TODO add the entire objects dataset and get rid of rings, food and drink,
+## seeds object info, and crops object info. don't forget the raw files!
+## TODO consider joining crops and seeds object info to crops to make
+## one crops dataset, so the user doesn't have to deal with it
 ## TODO add a convenience dataset for mapping these categories to
 ## a human readable name
 ## TODO replace null crafting recipes with complex unlock event (might involve
@@ -7,6 +11,13 @@
 ## TODO add a convenience function for NPC dispositions for
 ## one-hot encoding relationships
 ## TODO add the -777 wild seeds recipe
+## TODO fix some column names
+## - price -> sell price for crops object information
+## - get rid of all the redundant english_name or english_description columns
+## TODO add info on geodes from objects using info here
+## Utility::getTreasureFromGeode() https://stardewvalleywiki.com/Modding:Object_data
+## TODO add convenience dataset for categories and replace categories
+## in other datasets using info https://stardewvalleywiki.com/Modding:Object_data
 
 ## RESEARCH TODO's:
 ## TODO: Remember to update links to the wiki in the documentation once
