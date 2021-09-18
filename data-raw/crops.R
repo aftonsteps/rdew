1882497aab50985f67feb460aa6fb93755b3b267
aftonsteps <aftoncoombs@gmail.com>
2021-09-17 21:45:46 -0700

    Added crop id and name to crops
    - Added the resulting crop object id and crop name to the crops dataset
    - Updated documentation
    - Resolves #6

--- data-raw/crops.R
+++ data-raw/crops.R
@@ -21,6 +21,76 @@ crops <-
   dplyr::select(object_id,
                 dplyr::starts_with(match = "days_in_stage"),
                 dplyr::starts_with(match = "growth_season"),
-                dplyr::everything())
+                dplyr::everything()) %>%
+  dplyr::left_join(objects %>% dplyr::select(object_id, name),
+                   by = "object_id") %>%
+  dplyr::rename(seed_name = name)
+
+seed_pattern <- " Seeds$| Starter$| Tuber$ | Shoot$"
+
+multi_seeds <-
+  data.frame(crop_name_estimate =
+               rep(c("Spring", "Summer", "Fall", "Winter"), each=4),
+             real_name =
+               c("Wild Horseradish", "Daffodil", "Leek", "Dandelion",
+                 "Grape", "Spice Berry", "Sweet Pea", NA,
+                 "Blackberry", "Common Mushroom", "Hazelnut", "Wild Plum",
+                 "Crocus", "Crystal Fruit", "Snow Yam", "Winter Root")) %>%
+  dplyr::filter(!is.na(real_name))
+
+special_match_lookup <-
+  data.frame(crop_name_estimate =
+               c("Cranberry", "Bean", "Spangle",
+                 "Ancient", "Taro", "Pepper",
+                 "Fairy", "Cactus", "Jazz"),
+             real_name =
+               c("Cranberries", "Green Bean", "Summer Spangle",
+                 "Ancient Fruit", "Taro Root", "Hot Pepper",
+                 "Fairy Rose", "Cactus Fruit", "Blue Jazz")) %>%
+  dplyr::full_join(multi_seeds,
+                   by = c("crop_name_estimate", "real_name"))
+
+
+seed_names <-
+  crops %>%
+  dplyr::select(object_id, seed_name) %>%
+  dplyr::mutate(crop_name_estimate =
+                  gsub(pattern = seed_pattern,
+                       replacement = "",
+                       x = seed_name)) %>%
+  dplyr::left_join(objects %>% dplyr::select(object_id, name),
+                   by = c("crop_name_estimate" = "name"),
+                   suffix = c("_seed", "_crop")) %>%
+  dplyr::rename(seed_object_id = object_id_seed,
+                crop_object_id = object_id_crop) %>%
+  dplyr::left_join(special_match_lookup,
+                   by = "crop_name_estimate") %>%
+  dplyr::mutate(crop_name_estimate =
+                  ifelse(test = is.na(crop_object_id),
+                         yes = real_name,
+                         no = crop_name_estimate)) %>%
+  dplyr::select(seed_object_id, seed_name, crop_name = crop_name_estimate) %>%
+  dplyr::left_join(objects %>% dplyr::select(crop_object_id = object_id,
+                                             name),
+                   by = c("crop_name" = "name"))
+
+crops <-
+  crops %>%
+  dplyr::left_join(seed_names, by = c("seed_name"))
+
+## Little checks
+## Expect true
+nrow(crops) == sum(crops$object_id == crops$seed_object_id)
+## Expect 0
+sum(is.na(crops$crop_object_id))
+
+crops <-
+  crops %>%
+  dplyr::select(-object_id) %>%
+  dplyr::select(seed_object_id, seed_name, dplyr::everything()) %>%
+  dplyr::mutate(is_probabalistic =
+                  ifelse(test = crop_name %in% multi_seeds$real_name,
+                         yes = TRUE,
+                         no = FALSE))
 
 usethis::use_data(crops, overwrite = TRUE)
